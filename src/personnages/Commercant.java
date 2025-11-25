bd9962a191c5ed75c7a9250f4b8df6d111235838
Rotsiniaina <randriantsimbanavalona@gmail.com>
2025-11-25 11:18:03 +0100

    Update Commercant.java

--- src/personnages/Commercant.java
+++ src/personnages/Commercant.java
@@ -1,20 +1,19 @@
 package personnages;
 
-public class Commercant extends Humain{
-	
-	public Commercant(String nom, int argent) {
-		super(nom, "thé", argent);
-	}
-	
-	public int seFaireExtorquer() {
-		int somme = getArgent();
-		setArgent(0);
-		parler("J’ai tout perdu! Le monde est trop injuste...");
-		return somme;
-	}
-	
-	public void recevoir(int argent) {
-		setArgent(getArgent() + argent);
-		parler(argent+ " sous ! Je te remercie généreux donateur!");
-	}
+public class Commercant extends Humain {
+
+    public Commercant(String nom, int argent) {
+        super(nom, "thÃ©", argent);
+    }
+
+    public int seFaireExtorquer() {
+        int argentPerdu = perdreToutArgent();
+        parler("Jâ€™ai tout perdu! Le monde est trop injuste...");
+        return argentPerdu;
+    }
+
+    public void recevoir(int montant) {
+        gagnerArgent(montant);
+        parler(montant + " sous ! Je te remercie gÃ©nÃ©reux donateur!");
+    }
 }
